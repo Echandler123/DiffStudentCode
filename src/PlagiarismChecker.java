4ecbe3fd5efa2ae471b74e4174aa31d909a5217e
Echandler123 <elijah.chandler@menloschool.org>
2025-03-05 10:13:27 -0800

    update

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -15,17 +15,22 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
+        // Create a 2D array to store the lengths of longest common subsequences
         int[][] arr = new int[doc1.length() + 1][doc2.length() + 1];
+        // Traverses the 2D array
         for(int i = 1; i <= doc1.length(); i++) {
-            for(int j = 1; j <= doc2.length(); j++ ) {
+            for(int j = 1; j <= doc2.length(); j++) {
+                // If the characters are the same, add 1 to the previous diagonal value
                 if(doc1.charAt(i - 1) == doc2.charAt(j - 1)) {
-                    arr[i][j] = arr[i -1][j- 1] +1;
+                    arr[i][j] = arr[i -1][j- 1] + 1;
                 }
+                // Otherwise, take the max of the previous row or column
                 else {
                     arr[i][j] = Math.max(arr[i - 1][j], arr[i][j - 1]);
                 }
             }
         }
+        // Return the last value in the 2D array which is the longest common subsequence
         return arr[doc1.length()][doc2.length()];
     }
 }

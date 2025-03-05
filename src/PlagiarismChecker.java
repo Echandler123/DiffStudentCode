4a13b39ec76abe9107b69cf633ad0b173cbf8a04
Echandler123 <elijah.chandler@menloschool.org>
2025-03-04 20:40:02 -0800

    update

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -16,15 +16,23 @@ public class PlagiarismChecker {
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
         String currentLongest = "";
-        String [][] arr = new String[doc1.length()][doc2.length()];
+        int[][] arr = new int[doc1.length()][doc2.length()];
         int longest = 0;
-        for(int i =0; i < doc1.length(); i++) {
-            for(int j =0; j < doc2.length();j++ ) {
-                if(doc1.charAt(i) == doc1.charAt(j)) {
-
+        for(int i = 1; i <= doc1.length(); i++) {
+            for(int j = 1; j <= doc2.length(); j++ ) {
+                if(doc1.charAt(i - 1) == doc2.charAt(j -1)) {
+                        arr[i][j]  = arr[i-1][j-1] +1;
+                    }
+                else {
+                    if(arr[i-1][j] > arr[i][j-1]) {
+                        arr[i][j] = arr[i-1][j];
+                    }
+                    else {
+                        arr[i][j] = arr[i][j-1];
+                    }
                 }
             }
         }
-        return 0;
+        return arr[doc1.length()][doc2.length()];
     }
 }
